376c0cc7f473325462e0b18db72a9a212139b9df
project-defiant <szymonszyszkowski@gmail.com>
2025-11-21 09:07:39 +0000

    feat: 1e6

--- 1e6.py
+++ 1e6.py
@@ -74,5 +74,49 @@ def _(np, plt, stats):
     return
 
 
+@app.cell
+def _(np, plt, stats):
+    # The weight of adult elephants in kilograms assuming Normal distribution
+    mu_elephant = 5400  # mean weight in kg
+    sigma_elephant = 800  # standard deviation in kg
+    sample_size3 = 1000000
+    dist_normal = stats.norm(loc=mu_elephant, scale=sigma_elephant).rvs(sample_size3)
+    # Plot the distribution
+    plt.hist(dist_normal, bins=100, density=True)
+    plt.title('Normal Distribution of Elephant Weights')
+    plt.xlabel('Weight (kg)')
+    plt.ylabel('Density')
+    plt.text(1000, 0.0004, f'Normal Distribution\nμ={mu_elephant}, σ={sigma_elephant}', fontsize=12)
+    plt.text(1000, 0.00038, f'Sample Size = {sample_size3}', fontsize=12)
+    plt.text(1000, 0.00036, f'Mean of Sample = {np.mean(dist_normal):.2f}', fontsize=12)
+    plt.text(1000, 0.00034, f'Std Dev of Sample = {np.std(dist_normal):.2f}', fontsize=12)
+    plt.text(1000, 0.00032, f'Variance of Sample = {np.var(dist_normal):.2f}', fontsize=12)
+    plt.show()
+    return
+
+
+@app.cell
+def _(np, plt, stats):
+    # The weight of adult humans in pounds assuming skew Normal distribution
+    a_skewness = np.arange(0, 20, 1)
+    sample_size4 = 1000000
+    fig, axes_tuple = plt.subplots(len(a_skewness), 1, figsize=(8, 20))
+    loc2 = 180  # mean weight in lbs
+    scale2 = 30  # standard deviation in lbs
+    for i, a in enumerate(a_skewness):
+        axes = axes_tuple[i]
+        dist_skewnorm = stats.skewnorm(a=a, loc=loc2, scale=scale2).rvs(sample_size4)
+        axes.hist(dist_skewnorm, bins=100, density=True)
+        axes.set_title(f'Skew Normal Distribution of Human Weights (a={a})')
+        axes.set_xlabel('Weight (lbs)')
+        axes.set_ylabel('Density')
+        _maxy = axes.get_ylim()[1]
+        _maxx = axes.get_xlim()[1]
+        axes.text(_maxx - 50, 0.01, f'Skew Normal Distribution\nα={a}, loc={loc2}, scale={scale2}', fontsize=10)
+
+    plt.show()
+    return
+
+
 if __name__ == "__main__":
     app.run()
